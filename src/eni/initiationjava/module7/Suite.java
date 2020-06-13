f22b332fa9d46ba2bb84dd5d11acbc1572ee5eb0
dyrits <dyrits@outlook.fr>
2020-06-13 12:34:46 +0200

    Minor update.

--- src/eni/initiationjava/module7/Suite.java
+++ src/eni/initiationjava/module7/Suite.java
@@ -7,7 +7,7 @@ package eni.initiationjava.module7;
 
 public class Suite {
     public static void main(String[] args) {
-        String initialSuite  = "4";
+        String initialSuite  = "1";
         updateSuite(initialSuite, 40);
     }
 
@@ -28,4 +28,29 @@ public class Suite {
         }
         if (depth > 0) { updateSuite(updatedSuite.toString(), depth - 1); }
     }
+
+    /* Méthode similaire alternative avec un boucle plutôt qu'une récursion:
+    public static void main(String[] args) {
+        String initialSuite  = "1";
+        int depth = 40;
+        while (depth > 0) {
+            System.out.println(initialSuite);
+            StringBuilder partialSuite = new StringBuilder();
+            StringBuilder updatedSuite = new StringBuilder();
+            int index = 0;
+            while (index < initialSuite.length()) {
+                if (partialSuite.length() == 0 || initialSuite.charAt(index) == partialSuite.charAt(partialSuite.length() - 1)) {
+                    partialSuite.append(initialSuite.charAt(index));
+                    index ++;
+                    if (index != initialSuite.length()) { continue; }
+                };
+                updatedSuite.append(partialSuite.length());
+                updatedSuite.append(partialSuite.charAt(0));
+                partialSuite = new StringBuilder();
+            }
+            initialSuite = updatedSuite.toString();
+            depth --;
+        }
+    }
+    */
 }
